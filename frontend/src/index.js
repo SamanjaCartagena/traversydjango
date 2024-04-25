2d5fd0e5afda8323898b3e0b8c0e7943bc66a0b8
Samanja Cartagena <c.samanja09@Pabs.local>
2024-04-25 01:31:50 -0400

    cors related issues

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import App from './components/App';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
